8f130a61e5bae6d504b655ac168635f7cd40efac
Najmus Sakib Rashid <1805066@ugrad.cse.buet.ac.bd>
2023-06-09 00:13:55 +0600

    6-9-23 commit

--- Script/script.js
+++ Script/script.js
@@ -1,11 +1,18 @@
 let left=-150;
+let bodyMarginLeft=50;
+let jobPostContainerWidth=900;
+
 
 const slideUp=()=>{
     setTimeout(()=>{
         if(left<50){
             left+=25;
+            bodyMarginLeft+=25;
+            jobPostContainerWidth-=25;
             // console.log(width);
             document.getElementById('side-bar').style=`left:${left}px`;
+            document.getElementById('body').style=`margin-left:${bodyMarginLeft}px`;
+            document.getElementById('job-post-container').style=`width:${jobPostContainerWidth}px`;
             slideUp();
         }
     },1);
@@ -15,8 +22,12 @@ const slideDown=()=>{
     setTimeout(()=>{
         if(left>-150){
             left-=25;
+            bodyMarginLeft-=25;
+            jobPostContainerWidth+=25;
             // console.log(width);
             document.getElementById('side-bar').style=`left:${left}px`;
+            document.getElementById('body').style=`margin-left:${bodyMarginLeft}px`;
+            document.getElementById('job-post-container').style=`width:${jobPostContainerWidth}px`;
             slideDown();
         }
     },1);
